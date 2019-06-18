a440864c75afc8412249cd2658921401d8b292f0
Kamil <32514182+xTeJk@users.noreply.github.com>
2019-06-19 01:45:59 +0200

    Create log.h

---
+++ SRC_GAME/game/log.h
@@ -0,0 +1,8 @@
+// FIND // ZNAJDŹ //
+
+void        CharLog(LPCHARACTER ch, DWORD dw, const char * c_pszText, const char * c_pszHint);
+
+
+// ADD UNDER // DODAJ POD //
+
+void        OkayEventLog(int dwPID, const char * c_pszText, int points);
