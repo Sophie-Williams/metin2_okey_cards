59b800b376f3609bf9456f96c1f3cc7315ae8045
Kamil <32514182+xTeJk@users.noreply.github.com>
2019-06-19 01:43:56 +0200

    Create cmd.cpp

---
+++ SRC_GAME/game/cmd.cpp
@@ -0,0 +1,21 @@
+// FIND // ZNAJDŹ //
+
+ACMD(do_cube);
+
+
+// ADD UNDER // DODAJ POD //
+
+ACMD(do_cards);
+
+
+===============================================================
+    
+
+// FIND // ZNAJDŹ //
+    
+{ "cube",                do_cube,                0,    POS_DEAD,    GM_PLAYER    },
+
+
+// ADD UNDER // DODAJ POD //
+
+{ "cards",                do_cards,                0,    POS_DEAD,    GM_PLAYER    },
