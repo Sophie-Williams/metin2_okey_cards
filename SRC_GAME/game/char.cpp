361c78d1d3ed2f7df9259194baec3b770b2c97be
Kamil <32514182+xTeJk@users.noreply.github.com>
2019-06-19 01:41:45 +0200

    Create char.cpp

---
+++ SRC_GAME/game/char.cpp
@@ -0,0 +1,9 @@
+// FIND // ZNAJDŹ //
+
+memset(&m_quickslot, 0, sizeof(m_quickslot));
+
+
+// ADD UNDER // DODAJ POD //
+
+memset(&character_cards, 0, sizeof(character_cards));
+memset(&randomized_cards, 0, sizeof(randomized_cards));
