5e1d76ec17f3965e6b01157a639872218a7e5f1b
Kamil <32514182+xTeJk@users.noreply.github.com>
2019-06-19 01:36:25 +0200

    Create PythonSlotWindow.h

---
+++ SRC_BINARY/EterPythonLib/PythonSlotWindow.h
@@ -0,0 +1,8 @@
+// FIND // ZNAJDŹ //
+
+void SetSlot(DWORD dwIndex, DWORD dwVirtualNumber, BYTE byWidth, BYTE byHeight, CGraphicImage * pImage, D3DXCOLOR& diffuseColor);
+
+
+// ADD UNDER // DODAJ POD //
+
+void SetCardSlot(DWORD dwIndex, DWORD dwVirtualNumber, BYTE byWidth, BYTE byHeight, const char * c_szFileName, D3DXCOLOR& diffuseColor);
